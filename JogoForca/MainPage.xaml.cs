75b066c1061dc4e3151e69135c24c57d299d1327
Jorge Luiz Andrade <jorgeluizandrade@outlook.com>
2015-11-26 00:46:02 -0200

    Initial commit

---
+++ JogoForca/MainPage.xaml.cs
@@ -0,0 +1,162 @@
+﻿using JogoForca.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JogoForca
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+		private string currentHangmanImgSource;
+		private Hangman hangman;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+		private void btnLetter_Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = (Button)sender;
+			char letter = ((string)btn.Tag)[0];
+
+			btn.IsEnabled = false;
+
+			if (hangman.TryLetter(letter))
+			{
+				FillLetterBoxes();
+			}
+			else
+			{
+				int nErrors = hangman.GetErrorCount();
+				ChangeHangmanPicture(nErrors);
+			}
+
+			return;
+		}
+
+		private void FillLetterBoxes()
+		{
+			int n = VisualTreeHelper.GetChildrenCount(StackWord);
+			char?[] guessedLetters = hangman.GetGuessedLetters();
+
+			for(int i = 0; i < n; i++)
+			{
+
+				if(guessedLetters[i] != null)
+				{
+					Grid g = (Grid)VisualTreeHelper.GetChild(StackWord, i);
+					TextBlock tb = (TextBlock)g.Children.OfType<TextBlock>().FirstOrDefault();
+
+					tb.Text = guessedLetters[i].ToString();
+				}
+
+			}
+		}
+
+		private async void ChangeHangmanPicture(int errors)
+		{
+
+			Uri uri;
+
+			switch (errors)
+			{
+				case 1:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_1.jpg");
+					break;
+				case 2:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_2.jpg");
+					break;
+				case 3:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_3.jpg");
+					break;
+				case 4:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_4.jpg");
+					break;
+				case 5:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_5.jpg");
+					break;
+				case 6:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_final.jpg");
+					break;
+				default:
+					uri = new Uri("ms-appx:///Assets/Graphics/hang_initial.jpg");
+					break;
+			}
+
+
+			ImageBrush img = new ImageBrush();
+			StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+
+
+			//Carregamento asíncrono da imagem, evita que a imagem "pisque"
+			using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+			{
+				BitmapImage bitmapImage = new BitmapImage();
+				await bitmapImage.SetSourceAsync(fileStream);
+				img.ImageSource = bitmapImage;
+				recHangman.Fill = img;
+			}
+
+		}
+
+		private void btnStart_Click(object sender, RoutedEventArgs e)
+		{
+			string word = tbPalavra.Text;
+
+			hangman = new Hangman(word);
+
+			tbPalavra.Visibility = Visibility.Collapsed;
+			btnStart.Visibility = Visibility.Collapsed;
+			txtInstruction.Visibility = Visibility.Collapsed;
+
+			CreateLettersBoxes();
+		}
+
+		private void CreateLettersBoxes()
+		{
+			StackPanel sp = StackWord;
+
+			int length = hangman.GetWordLength();
+
+			for (int i = 0; i < length; i++)
+			{
+				Grid g = new Grid();
+				g.Margin = new Thickness(2);
+				g.Height = 30;
+				g.Width = 30;
+				g.Background = Resources["SystemControlHighlightAccentBrush"] as Brush;
+
+				TextBlock txt = new TextBlock();
+				txt.Text = "";
+				txt.HorizontalAlignment = HorizontalAlignment.Center;
+				txt.VerticalAlignment = VerticalAlignment.Center;
+				txt.Foreground = Resources["AppBarBackgroundThemeBrush"] as Brush;
+
+				g.Children.Add(txt);
+				sp.Children.Add(g);
+			}
+		}
+	}
+}
---
+++ JogoForca/VO/Hangman.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoForca.VO
+{
+	class Hangman
+	{
+		private string word;
+		private char?[] guessedLetters;
+		private int errors;
+		private List<char> wrongLetters;
+		private List<char> correctLetters;
+
+		public Hangman(string word)
+		{
+			int size = word.Length;
+			guessedLetters = new char?[size];
+			this.word = word;
+
+			errors = 0;
+
+			for (int i = 0; i < size; i++)
+			{
+				guessedLetters[i] = null;
+			}
+		}
+
+		public char?[] GetGuessedLetters()
+		{
+			return guessedLetters;
+		}
+
+		public void SetWord(string word)
+		{
+			this.word = word;
+		}
+
+		public bool TryLetter(char letter)
+		{
+
+			bool guessedCorrectly = false;
+			int size = word.Length;
+
+			letter = letter.ToString().Normalize(NormalizationForm.FormD)[0];
+
+			for (int i = 0; i < size; i++)
+			{
+				//Converte o caracter que vai ser checado para letra maíscula e ignorando acentuação
+				char wordLetter = word[i].ToString().Normalize(NormalizationForm.FormD).ToUpper()[0];
+
+				if(letter == wordLetter)
+				{ 
+					guessedLetters[i] = word[i];
+					guessedCorrectly = true;
+				}
+
+			}
+
+			if (guessedCorrectly == false)
+				errors++;
+
+			return guessedCorrectly;
+		}
+
+		public int GetWordLength()
+		{
+			return word.Length;
+		}
+
+		public int GetErrorCount()
+		{
+			return errors;
+		}
+	}
+}
+
+		
